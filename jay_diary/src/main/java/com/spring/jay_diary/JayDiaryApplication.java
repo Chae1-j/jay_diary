274c5f90c041b3e640162fd0b36c4fade9c16811
Chae1-j <jcw3241@gmail.com>
2024-10-30 13:35:45 +0900

    1030 프로젝트 생성 리포지토리 업로드
    생성프로젝트를 빈 리포지토리에 업로드한다.

---
+++ jay_diary/src/main/java/com/spring/jay_diary/JayDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jay_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JayDiaryApplication.class, args);
+	}
+
+}
---
+++ jay_diary/src/test/java/com/spring/jay_diary/JayDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jay_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
